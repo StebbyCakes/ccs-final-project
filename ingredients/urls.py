5d35f234a4973a2852c3de583dbc2a17cad6b8f9
Esteban Carter <carterea1999@gmail.com>
2021-07-07 17:34:54 -0400

    this push doesnt work but need to see what I changed from last push bc/ that one worked

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -5,6 +5,6 @@ from .views import IngredientListAPIView, IngredientEditAPIView
 app_name = 'ingredient'
 
 urlpatterns = [
-    path('<int:pk/', IngredientEditAPIView.as_view(), name = 'edit_ingredient'),
+    path('<int:pk>/', IngredientEditAPIView.as_view(), name = 'edit_ingredient'),
     path('', IngredientListAPIView.as_view(), name = "ingredient_list"),
 ]
