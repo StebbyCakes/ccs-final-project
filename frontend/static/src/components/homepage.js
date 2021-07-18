89cb51073bdfa64444fb3a6da7684a0d6ddb279f
Esteban Carter <carterea1999@gmail.com>
2021-07-18 16:58:34 -0400

    trying to implement graph

--- frontend/static/src/components/homepage.js
+++ frontend/static/src/components/homepage.js
@@ -6,6 +6,43 @@ import Navbar from 'react-bootstrap/Navbar';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const { REACT_APP_SPOONACULAR_API_KEY } = process.env
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+
+  }
+];
 
 class Homepage extends Component {
   constructor(props) {
@@ -69,17 +106,40 @@ class Homepage extends Component {
     const displayMenuItems = this.state.menuitems.map((menuitem) => {
       console.log(this.calculateMenuItemCost(menuitem))
       return (
-        <div>
+        <div className='menuCost'>
         {menuitem.name}
-        {this.calculateMenuItemCost(menuitem)}
+        <h1></h1>
+        ${this.calculateMenuItemCost(menuitem)}
+        <p></p>
       </div>
     )})
 
     return(
       <>
-        <div>
-          {displayMenuItems}
+        <div >
+          <ul >
+            {displayMenuItems}
+          </ul>
         </div>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChart>
 
       </>
     )}}
